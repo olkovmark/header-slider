c630bbc97b83ff2f87109222d2513945d6dcd83b
olkovmark <olkovmark@gmail.com>
2023-09-13 12:22:32 +0200

    update

--- src/route/index.js
+++ src/route/index.js
@@ -1,14 +1,12 @@
-// Підключаємо роутер до бек-енду
 const express = require('express')
 const router = express.Router()
 
-// Підключіть файли роутів
-const test = require('./test')
-// Підключіть інші файли роутів, якщо є
+router.get('/', function (req, res) {
+  res.render('calc', {
+    style: 'calc',
+    name: 'calc',
+    title: 'Калькулятор',
+  })
+})
 
-// Об'єднайте файли роутів за потреби
-router.use('/', test)
-// Використовуйте інші файли роутів, якщо є
-
-// Експортуємо глобальний роутер
 module.exports = router
