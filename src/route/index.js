e3664f7d03d046db96f30844f3a61cc609a1a618
olkovmark <olkovmark@gmail.com>
2023-09-13 14:05:52 +0200

    s

--- src/route/index.js
+++ src/route/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/nav', function (req, res) {
+  res.render('nav', {
+    style: 'nav',
+    name: 'nav',
+    title: 'Nav',
+  })
+})
+
 router.get('/', function (req, res) {
-  res.render('calc', {
-    style: 'calc',
-    name: 'calc',
-    title: 'Калькулятор',
+  res.render('scroll', {
+    style: 'scroll',
+    name: 'scroll',
+    title: 'scroll',
   })
 })
 
